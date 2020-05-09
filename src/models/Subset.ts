3247abf784d5ea6db343f74103b3325eb71e5abd
Clayton Fidelis <clayton.fidelis@live.com>
2020-05-09 11:18:09 +0200

    fix: add subset image

--- src/models/Subset.ts
+++ src/models/Subset.ts
@@ -1,7 +1,8 @@
 export default interface Subset {
-  id: string;
-  name: string;
-  objectsCount: number;
-  collectionsCount: number;
-  museumsCount: number;
+  id: string
+  name: string
+  objectsCount: number
+  collectionsCount: number
+  museumsCount: number
+  image: string
 }
